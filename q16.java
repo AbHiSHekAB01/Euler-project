0f3a8b60cd920d00625d6cb6cd50154623585bba
AbHiSHekAB01 <66764663+AbHiSHekAB01@users.noreply.github.com>
2020-06-11 10:33:01 +0530

    Update q16.java

--- q16.java
+++ q16.java
@@ -1,3 +1,8 @@
+//Q16) 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+//
+//     What is the sum of the digits of the number 2^1000?
+
+
 package eulerProject;
 
 import java.math.BigInteger;
