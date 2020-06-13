bcd63d2fb0a596f078bc11bfc9ba275991ac050b
AbHiSHekAB01 <66764663+AbHiSHekAB01@users.noreply.github.com>
2020-06-13 18:17:54 +0530

    Update q3.java

--- q3.java
+++ q3.java
@@ -6,20 +6,21 @@ package eulerProject;
 
 public class q3{
 
-	public static void main(String[] args) {
-		
-		double n= 600851475143d;
-		for  (int i=2; i<n/2; i++)
-		{
-			while(n%i==0)
-			{
-				n=n/i;
-			}
-
+	public static void main(String[] args)  {
+		 double n=600851475143.0;double max=0; int c=0,s = 0;
+		for(int i=2;i<=Math.sqrt(n);i++) {
+			if(n%i==0) {
+				if(i>1) {
+					for(int j=2;j<=Math.sqrt(i);j++) {
+						if(i%j==0)
+							c++;
+					}if(c==0)
+						s=i;
+				}
 		}
 
-		System.out.println(n);
-		}
+	}System.out.println(s);
+	}
 	}
 
 //------------------output--------------------------------------------------
